14a075699758b88758c4f004fc814b6e22a9cb47
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-21 02:51:03 +0300

    design updated

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/addPhoto', (req, res) => {
+  res.render('addPhoto');
+});
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda baslatildi`);
