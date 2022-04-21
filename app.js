b0bee28cf0165dd6ea7b9f86097055e11d5a6220
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-21 23:35:56 +0300

    routing added

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose'); 
+
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
+
 const app = express();
 
+//db connection
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//template engine
 app.set('view engine', 'ejs');
+
+//middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -16,8 +33,12 @@ app.get('/about', (req, res) => {
 app.get('/addPhoto', (req, res) => {
   res.render('addPhoto');
 });
+app.post('/photos', async(req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 
-
+//server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda baslatildi`);
